07679b38c91853e5eee9e4b1d5b3daa1174fa52b
psyberd1001 <108468415+psyberd1001@users.noreply.github.com>
2024-03-21 20:16:03 +0300

    Add files via upload

---
+++ homework18.py
@@ -0,0 +1,6 @@
+def print_1(*args):
+    print('Hello, world')
+
+def print_2(*args):
+    print('Hello, world!')
+
---
+++ main.py
@@ -0,0 +1,4 @@
+from homework18 import print_1 as pr1, print_2 as pr2
+
+pr1()
+pr2()
